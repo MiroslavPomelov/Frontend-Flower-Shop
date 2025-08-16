4d94006e7c0c2753922b9abb47c0dc017b8afec6
Miroslav Pomelov <128490138+MiroslavPomelov@users.noreply.github.com>
2025-08-16 11:40:30 +0200

    ы

--- flower-shop/next.config.js
+++ flower-shop/next.config.js
@@ -1,14 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	eslint: {
-		ignoreDuringBuilds: true,
-	},
-	typescript: {
-		ignoreBuildErrors: true,
-	},
-};
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  experimental: {
+    esmExternals: false
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
 
 
 
